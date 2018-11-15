4d0b706a8e305ed35c14d3f9f31285f062997526
Grigorieva1Dasha <32798782+Grigorieva1Dasha@users.noreply.github.com>
2018-11-15 17:41:19 +0800

    Update array.cpp

--- array.cpp
+++ array.cpp
@@ -9,12 +9,19 @@ Array::Array(int startCapacity){
 		capacity = startCapacity;
 	ptr = new int[capacity];
 	size = 0;
-}Array::~Array() {
-	delete[] ptr;}Array::Array(const Array& arr) {	ptr = new int[arr.capacity];
+}
+Array::~Array() {
+	delete[] ptr;
+}
+Array::Array(const Array& arr) {
+	ptr = new int[arr.capacity];
 	size = arr.size;
 	capacity = arr.capacity;
 	for (int i = 0; i<size; i++)
-		ptr[i] = arr.ptr[i];}Array& Array::operator=(const Array& arr) {	if (this == &arr)
+		ptr[i] = arr.ptr[i];
+}
+Array& Array::operator=(const Array& arr) {
+	if (this == &arr)
 		return *this;
 	if (capacity != arr.capacity){
 		delete[] ptr;
@@ -24,12 +31,15 @@ Array::Array(int startCapacity){
 	size = arr.size;
 	for (int i = 0; i<size; i++)
 		ptr[i] = arr.ptr[i];
-	return *this;}int& Array::operator [](int index){
+	return *this;
+}
+int& Array::operator [](int index){
 	if (index >= size || index < 0)
 		throw ArrayException();
 	else
 		return ptr[index];
-}void Array::insert(int elem, int index){
+}
+void Array::insert(int elem, int index){
 	if (index < 0 || index > size)
 		throw ArrayException();
 	if (size == capacity)
@@ -41,25 +51,35 @@ Array::Array(int startCapacity){
 }
 void Array::insert(int elem){
 	insert(elem, size);
-}void Array::increaseCapacity(int newCapacity){	capacity = newCapacity < capacity * 2 ?
+}
+void Array::increaseCapacity(int newCapacity){
+	capacity = newCapacity < capacity * 2 ?
 		capacity * 2 : newCapacity;
 	int* newPtr = new int[capacity];
 	for (int i = 0; i<size; i++)
 		newPtr[i] = ptr[i];
 	delete[] ptr;
-	ptr = newPtr;}void Array::remove(int index){
+	ptr = newPtr;
+}
+void Array::remove(int index){
 	if (index < 0 || index >= size)
 		throw ArrayException();
 	for (int j = index; j<size - 1; j++)
 		ptr[j] = ptr[j + 1];
 	ptr[size - 1] = 0;
 	size--;
-}int Array::getSize() const {	return size;}ostream& operator <<(ostream& out, const Array& arr){
+}
+int Array::getSize() const {
+	return size;
+}
+ostream& operator <<(ostream& out, const Array& arr){
 	out << "Total size: " << arr.size << endl;
 	for (int i = 0; i<arr.size; i++)
 		out << arr.ptr[i] << endl;
 	return out;
-}int main() {	setlocale(LC_ALL, "Russian");
+}
+
+int main() {
 	Array arr(4);
 	for (int i = 0; i<4; i++)
 		arr.insert(i + 1);
@@ -72,4 +92,7 @@ void Array::insert(int elem){
 	cout << arr << endl;
 	for (int i = 6; i >= 0; i -= 3)
 		arr.remove(i);
-	cout << arr << endl;	system("pause");	return 0;}
+	cout << arr << endl;
+	system("pause");
+	return 0;
+}
